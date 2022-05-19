8d05eb1d5d709010827234f0d040b705ff39b5be
EduardoGarridoRdz <54334525+EduardoGarridoRdz@users.noreply.github.com>
2022-05-18 21:30:05 -0500

    Update reservar.php

--- proyecto/reservar.php
+++ proyecto/reservar.php
@@ -16,13 +16,14 @@
 <html>
 <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
 <link rel="stylesheet" type="text/css" href="styles/style.css">
+<link rel="stylesheet" type="text/css" href="styles/styleform.css">
 <script type="text/javascript" src="js/bootstrap.min.js"></script>
 <script type="text/javascript" src="js/scripts.js"></script>
 
 <meta>
 
 <head>
-    <title>APRENDE IDIOMAS</title>
+    <title>Reservación de laboratorios</title>
 </head>
 
 <body class="body1">
@@ -32,14 +33,25 @@
             <div class="logo">
             <img src="styles/imagenes/logo.png" height= 80, weight = 244>
             </div>
-
             <nav>
                 <a href="index.php">Horario</a>
                 <a href="reservar.php">Reservar laboratorio</a>
                 <a href="solicitud.php">Solicitud de reservaciones</a>
-                <a href="inicio.php">Iniciar sesion</a>
+                <a href="inicio.php">Cerrar sesión</a>
             </nav>
         </div>
     </header>
+    <section class="form-login">
+        <h5>Reservar laboratorio</h5>
+        <form action="" >
+            <input class="controls" type="text" name="nombre" placeholder="Nombre">
+            <input class="controls" type="text" name="ap_paterno" placeholder="Apellido paterno">
+            <input class="controls" type="text" name="ap_materno" placeholder="Apellido materno">
+            <input class="controls" type="text" name="correo" placeholder="Correo">
+            <input class="controls" type="text" name="clase" placeholder="Nombre de la clase">
+            <input class="controls" type="text" name="horario" placeholder="Horario">
+            <input class="buttons" type="submit" name="" value="Ingresar">
+        </form>
+    </section>
 </body>
 </html>
